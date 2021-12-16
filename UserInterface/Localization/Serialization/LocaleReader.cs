7a6560bbedb4dd3204a79f1e1d7d1b2355c5b8ad
User <85743764+TheCSUser@users.noreply.github.com>
2021-12-16 21:02:35 +0100

    improve loading of localization

--- UserInterface/Localization/Serialization/LocaleReader.cs
+++ UserInterface/Localization/Serialization/LocaleReader.cs
@@ -12,6 +12,37 @@ namespace com.github.TheCSUser.Shared.UserInterface.Localization.Serialization
 
     public sealed class LocaleReader : WithContext
     {
+        private static readonly string[] EMPTY_CHARACTERS = new[] {
+            "\u0009",
+            "\u000b",
+            "\u000c",
+            "\u0085",
+            "\u00a0",
+            "\u1680",
+            "\u180e",
+            "\u2000",
+            "\u2001",
+            "\u2002",
+            "\u2003",
+            "\u2004",
+            "\u2005",
+            "\u2006",
+            "\u2007",
+            "\u2008",
+            "\u2009",
+            "\u200a",
+            "\u200b",
+            "\u200c",
+            "\u200d",
+            "\u2028",
+            "\u2029",
+            "\u202f",
+            "\u205f",
+            "\u2060",
+            "\u3000",
+            "\ufeff"
+        };
+
         private readonly XmlSerializer _serializer;
 
         internal LocaleReader(IModContext context) : base(context)
@@ -41,7 +72,7 @@ namespace com.github.TheCSUser.Shared.UserInterface.Localization.Serialization
                     }
                     library.Add(
                         GetFileNameWithoutExtension(fileName).Split('.').Last().ToLower(),
-                        new LanguageDictionary(Context, file.ToDictionary(item => item.Name, item => item.Value)));
+                        new LanguageDictionary(Context, file.ToDictionary(item => item.Name, item => PreProcess(item.Value))));
                 }
                 return library;
             }
@@ -50,6 +81,14 @@ namespace com.github.TheCSUser.Shared.UserInterface.Localization.Serialization
                 Log.Error($"{nameof(LocaleReader)}.{nameof(Load)} failed", e);
                 return new Library();
             }
+        }
+
+        private string PreProcess(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return string.Empty;
+            return EMPTY_CHARACTERS
+                .Aggregate(text, (currentText, character) => currentText.Replace(character, " "))
+                .Trim();
         }
     }
 }
